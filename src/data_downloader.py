300825f4a4f09552e30ee11bd556b4a6626ae28b
yb-ai-research <youngbin.kim.yb@gmail.com>
2024-03-10 13:11:23 -0700

    explore

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -8,7 +8,7 @@ def fetch_spam_data():
     ham_url = spam_root + "20030228_easy_ham.tar.bz2"
     spam_url = spam_root + "20030228_spam.tar.bz2"
 
-    spam_path = Path() / "datasets" / "spam"
+    spam_path = Path() / ".." / "datasets" / "spam"
     spam_path.mkdir(parents=True, exist_ok=True)
     for dir_name, tar_name, url in (("easy_ham", "ham", ham_url),
                                     ("spam", "spam", spam_url)):
@@ -20,6 +20,7 @@ def fetch_spam_data():
             tar_bz2_file.extractall(path=spam_path)
             tar_bz2_file.close()
     return [spam_path / dir_name for dir_name in ("easy_ham", "spam")]
+
 
 if __name__ == "__main__":
     fetch_spam_data()
